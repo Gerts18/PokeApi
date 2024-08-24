811b448efcc672092c48a48639426da2273cb4ac
German <germants100@gmail.com>
2024-08-24 17:52:59 -0600

    Finishing main info pokemon

--- src/pages/PokeDetails/PokemonDetails.jsx
+++ src/pages/PokeDetails/PokemonDetails.jsx
@@ -3,6 +3,8 @@ import { PokemonContext } from '@/context/PokemonContext'
 import { useParams } from 'react-router-dom'
 import styles from './PokemonDetails.module.css'
 import pokeball from '@/assets/pokeball_colored.png'
+import male from '@/assets/male_icon.svg'
+import female from '@/assets/female_icon.svg'
 
 const PokemonDetails = () => {
 
@@ -12,31 +14,52 @@ const PokemonDetails = () => {
 
   const [pokemonImage, setPokemonImage] = useState('')
   const [species, setSpecies] = useState(null)
+  const [types, setTypes] = useState([])
+  const [weaknesses, setWeaknesses] = useState([])
 
   const [url, setUrl] = useState(null);
   const [data, setData] = useState(null);
 
   const pokemon = pokemonsList.find(pokemon => pokemon.id == id);
 
+  /*To handle data from the pokemon */
   useEffect(() => {
     if (pokemon) {
       setPokemonImage(pokemon.sprites.other['official-artwork'].front_default);
+      setTypes(pokemon.types.map(typeInfo => typeInfo.type.url))
     }
   }, [pokemon]);
 
+  /*To handle individual requests */
   useEffect(() => {
+    if (!pokemon) return;
+
     const fetchData = async () => {
-      if (url) {
-        const request = await requestData(url);
-        setData(request);
+      const urls = [
+        {
+          url: pokemon.species.url,
+          set_data: setSpecies
+        },
+        ...types.map(typeObj => ({
+          url: typeObj,
+          set_data: (obj) => {
+            setWeaknesses ( (prev) => {
+              const newWeaknesses = obj.damage_relations.double_damage_from.map((type) => type.name);
+              return [...new Set([...prev, ...newWeaknesses])];
+            } )
+          }
+        }))
+      ];
+
+      for (const { url, set_data } of urls) {
+        const response = await requestData(url);
+        set_data(response);
       }
     };
-    fetchData();
-  }, [url]);
 
-  useEffect(() => {
-    handleRequests();
-  }, [pokemon, data]);
+    fetchData();
+  }, [pokemon, types, requestData]);
+
 
   const handleVersions = (versions) => {
     const sprites = []
@@ -67,33 +90,39 @@ const PokemonDetails = () => {
     )
   }
 
-  const handleRequests = async () => {
-    if (pokemon) {
-      /*Here i can put individual urls to made request to get specific data */
-      const urls = [
-        {
-          url: pokemon.species.url,
-          set_data: setSpecies
-        }
-      ];
+  const handleHeight = (dm) => {
+    //From decimeters to meters
+    const meters = dm * 0.1;
+    
+    // From meters to feet
+    const totalFeet = meters * 3.28084;
+  
+    // Feet
+    const feet = Math.floor(totalFeet);
+  
+    // Inches
+    const inches = Math.round((totalFeet - feet) * 12);
+  
+    return `${feet}' ${inches}''`;
+  }
 
-      for (const urlObj of urls) {
-        setUrl(urlObj.url);
-
-        /*Wait for data to be available after the url change */
-        await new Promise(resolve => {
-          const checkData = setInterval(() => {
-            if (data) {
-              clearInterval(checkData);
-              urlObj.set_data(data);
-              console.log(data)
-              resolve();
-            }
-          }, 100);
-        });
+  const handleGender = (rate) => {
+      switch (rate){
+        case -1:
+          return <p className={styles.infoDescription} >Unknown</p>
+        case 0:
+          return <img className={styles.genderImg} src={male} />
+        case 8:
+          return <img className={styles.genderImg} src={female} />
+        default:
+          return (
+            <div style={{display:'flex'}}  >
+              <img className={styles.genderImg} src={male}/>
+              <img className={styles.genderImg} src={female} />
+            </div>
+          )
       }
-    }
-  };
+  }
 
   return (
     <>
@@ -110,6 +139,7 @@ const PokemonDetails = () => {
 
               <div className={styles.pokemonStats}>
                 <img className={styles.pokeImage} src={pokemonImage} alt="" />
+
                 <div className={styles.stats}>
                   {
                     pokemon.stats.map((stat, index) => {
@@ -119,12 +149,13 @@ const PokemonDetails = () => {
                     })
                   }
                 </div>
+                
               </div>
 
               <div className={styles.pokemonDescription}>
 
                 <p>
-                   {species && species.flavor_text_entries[0].flavor_text.replace(/\f/g, ' ')}
+                  {species && species.flavor_text_entries[2].flavor_text.replace(/\f/g, ' ')}
                 </p>
 
                 <div className={styles.mainVersionsContainer}>
@@ -140,52 +171,96 @@ const PokemonDetails = () => {
                     }
                   </div>
 
-                    
+
                 </div>
 
                 <div className={styles.generalInformation}>
-                      <div className={styles.infoItem}>
-                      <span className={styles.info}>Height</span>
-                      <p className={styles.infoDescription}>20.40</p>
-                      </div>
+                  <div className={styles.infoItem}>
+                    <span className={styles.info}>Height</span>
+                    <p className={styles.infoDescription}>
+                       {handleHeight(pokemon.height)} 
+                    </p>
+                  </div>
 
-                      <div className={styles.infoItem}>
-                      <span className={styles.info}>Category</span>
-                      <p className={styles.infoDescription}>Seed</p>
+                  <div className={styles.infoItem}>
+                    <span className={styles.info}>Category</span>
+                      <div style={{
+                        display: 'flex', gap: '5px'
+                      }}>
+                      {
+                        species &&
+                        species.egg_groups.map((egg, index) => {
+                          return (
+                            <p key={index} className={styles.infoDescription}>
+                              *{refactorDetails('name', egg.name)}
+                            </p>
+                          )
+                        })
+                      }
                       </div>
+                  </div>
 
-                      <div className={styles.infoItem}>
-                      <span className={styles.info}>Weight</span>
-                      <p className={styles.infoDescription}>15.2 lb</p>
-                      </div>
+                  <div className={styles.infoItem}>
+                    <span className={styles.info}>Weight</span>
+                    <p className={styles.infoDescription}>
+                       {`${Math.round(pokemon.weight/4.53592 *100)/100} lbs`} 
+                    </p>
+                  </div>
 
-                      <div className={styles.infoItem}>
-                      <span className={styles.info}>Abilities</span>
-                      <p className={styles.infoDescription}>Overgrow</p>
-                      </div>
+                  <div className={styles.infoItem}>
+                    <span className={styles.info}>Abilities</span>
+                    <p className={styles.infoDescription}> 
+                      { refactorDetails('name', pokemon.abilities[0].ability.name ) }
+                    </p>
+                  </div>
 
-                      <div className={styles.infoItem}>
-                      <span className={styles.info}>Gender</span>
-                      <p className={styles.infoDescription}>none</p>
-                      </div>  
-                      
+                  <div className={styles.infoItem}>
+                    <span className={styles.info}>Gender</span>
+                      {
+                        species && 
+                        handleGender(species.gender_rate)
+                      }
+                  </div>
+
 
                 </div>
 
-                <div className={styles.typesContainer}>
-                  {
-                    pokemon.types.map((type, index) => {
-                      return (
-                        <p
-                          key={index}
-                          style={{ backgroundColor: typeColors[type.type.name] }}
-                          className={styles.type}
-                        >
-                          {refactorDetails('name', type.type.name)}
-                        </p>
-                      )
-                    })
-                  }
+                <div>
+                  <h2>Type</h2>
+                  <div className={styles.typesContainer}>
+                    {
+                      pokemon.types.map((type, index) => {
+                        return (
+                          <p
+                            key={index}
+                            style={{ backgroundColor: typeColors[type.type.name] }}
+                            className={styles.type}
+                          >
+                            {refactorDetails('name', type.type.name)}
+                          </p>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+
+                <div>
+                  <h2>Weaknesses</h2>
+                  <div className={styles.typesContainer}>
+                    {weaknesses &&
+                      weaknesses.map((weak, index) => {
+                        return (
+                          <p
+                            key={index}
+                            style={{ backgroundColor: typeColors[weak] }}
+                            className={styles.type}
+                          >
+                            {refactorDetails('name', weak)}
+                          </p>
+                        )
+                      })
+                      }
+                  </div>
                 </div>
 
               </div>
